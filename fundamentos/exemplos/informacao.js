6a05b1602f0253ac6b3a96d61391c6f6ceb07b0c
Matheus Raymundo <matheus.raymundo@aluno.senai.br>
2026-02-06 09:54:40 -0300

    as

---
+++ fundamentos/exemplos/informacao.js
@@ -0,0 +1,12 @@
+    const Curso = "Técnico em Desenvolvimento de Sistemas"
+    const ano = 2025
+    const notaMínimaParaSerAprovado = 50
+    const notaParaSerReprovado = 49.9
+    const cursoTecnico = true
+
+    console.log('Minhas informações Acadêmicas')
+    console.log("Curso " + Curso)
+    console.log(`Ano: ${ano}`)
+    console.log(`Nota para aprovação ${notaMínimaParaSerAprovado}`)
+    console.log(`Nota mínima para ser reprovado ${notaParaSerReprovado}`)
+    console.log(`É curso técnico: ${cursoTecnico}`)
