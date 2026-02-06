2b4870712f3f3ffed6c3d114f5cf7852beb9ade5
Matheus Raymundo <matheus.raymundo@aluno.senai.br>
2026-02-06 10:02:40 -0300

    nota

--- fundamentos/exemplos/nota.js
+++ fundamentos/exemplos/nota.js
@@ -1,1 +1,7 @@
 const aluno = prompt("Qual nome do aluno")
+const nota1 = parseFloat(prompt("Qual a primeira nota?"))
+const nota2 = parseFloat(prompt("Qual a segunda nota?"))
+
+const media = (nota1 + nota2) / 2
+
+alert(`A media das notas ${nota1} e ${nota2} é ${media} `)
